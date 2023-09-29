e87f36286f13ea4a32aa26a9627458d50ba3e431
DiyawanDawan <diyawan0@gmail.com>
2023-09-29 10:24:26 +0800

    Melalukan Update kode pada componen dan style css menambahkan shedow.dom pada berkas file javascrip componen

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -4,7 +4,7 @@ class DataSource {
   static searchClub(keyword) {
     return new Promise((resolve, reject) => {
       const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
-      
+
       if (filteredClubs.length) {
         resolve(filteredClubs);
       } else {
